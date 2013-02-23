60466009bc0641697af3faf718d8388b52450fbb
Raghavendra Belapure <raghavendra@raghavendra-laptop>
2013-02-23 12:33:05 -0500

    implemented various services in eventservice

--- README.md
+++ README.md
@@ -5,16 +5,22 @@ Webservice backend support for Learn-Over-Lunch Android App
 
 Communication Protocol :
 
-Endpoint		Consume					Produce
-=================================================================================================
+Endpoint			Consume					Produce
+==================================================================================================================
 /userservice
-	/auth		(str) <uname>::<passwd>			(str) RESP_YES / RESP_NO
-	/signup		(json) <full profile>			(str) RESP_YES / RESP_NO
-	/profile	(json) myuname, profileuname		(json) errorcode, isFriend, <full profile>
+	/auth			(str) <uname>::<passwd>			(str) RESP_YES / RESP_NO
+	/signup			(json) <full profile>			(str) RESP_YES / RESP_NO
+	/profile		(json) myuname, profileuname		(json) errorcode, isFriend, <full profile>
 	/friend
-		/get	(str) uname				(jsonarr) arr of jsons - profiles
-		/add	(json) myuname, profileuname		(str) RESP_NO / RESP_YES / RESP_MALFORMED
-		/accept	(json) myuname, profileuname		(str) RESP_NO / RESP_YES / RESP_MALFORMED
+		/get		(str) uname				(jsonarr) arr of jsons - profiles
+		/add		(json) myuname, profileuname		(str) RESP_NO / RESP_YES / RESP_MALFORMED
+		/accept		(json) myuname, profileuname		(str) RESP_NO / RESP_YES / RESP_MALFORMED
 
 /eventservice
-	/create		(json) <event details, uname>		(str) RESP_MALFORMED / RESP_YES / RESP_NO
+	/create			(json) <event details, uname>		(str) RESP_MALFORMED / RESP_YES / RESP_NO
+	/get
+	     /event
+	    	/byuser		(str) uname				(json) arr of jsons - events
+	    	/bycategory	(str) uname				(json) arr of jsons - events
+	     /members		(str) eventid				(json) arr of jsons - profiles
+	/isMember		(json) uname, event_id			(str) RESP_NO / RESP_YES / RESP_MALFORMED
--- src/edu/gatech/mas/learnoverlunch/service/EventMgmtService.java
+++ src/edu/gatech/mas/learnoverlunch/service/EventMgmtService.java
@@ -10,6 +10,7 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import org.codehaus.jettison.json.JSONArray;
 import org.codehaus.jettison.json.JSONException;
 import org.codehaus.jettison.json.JSONObject;
 
@@ -67,5 +68,186 @@ public class EventMgmtService {
 			}
 		}
 		return Constants.RESP_YES;
+	}
+	
+	@Path("/get/event/byuser")
+	@POST
+	@Consumes(MediaType.TEXT_PLAIN)
+	@Produces(MediaType.APPLICATION_JSON)
+	public String getEventByUsername(String uname)
+	{
+		JSONArray arr = new JSONArray();
+		JSONObject o;
+		Connection conn = DatabaseHandler.getConnection();
+		Statement st = null;
+		ResultSet rset;
+		
+		try {
+			st = (Statement) conn.createStatement();
+			rset = st.executeQuery("select * from events_mst " +
+					"where event_id in " +
+					"(select event from event_attendees " +
+					"where event_members='" + uname + "');");
+			while(rset.next()){
+				o = new JSONObject();
+				o.put("event_id", rset.getInt("event_id"));
+				o.put("event_date",rset.getString("event_date"));
+				o.put("event_place",rset.getString("event_place"));
+				o.put("topic_name", rset.getString("topic_name"));
+				o.put("topic_category",rset.getString("topic_category"));
+				o.put("max_allowed_members", rset.getInt("max_allowed_members"));
+				arr.put(o);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} catch (JSONException e) {
+			e.printStackTrace();
+		} finally
+		{
+			try {
+				st.close();
+				conn.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		return arr.toString();
+	}
+	
+	@Path("/get/event/bycategory")
+	@POST
+	@Consumes(MediaType.TEXT_PLAIN)
+	@Produces(MediaType.APPLICATION_JSON)
+	public String getEventByCategory(String category)
+	{
+		JSONArray arr = new JSONArray();
+		JSONObject o;
+		Connection conn = DatabaseHandler.getConnection();
+		Statement st = null;
+		ResultSet rset;
+		
+		try {
+			st = (Statement) conn.createStatement();
+			rset = st.executeQuery("select * from events_mst " +
+					"where topic_category='" + category + "';");
+			while(rset.next()){
+				o = new JSONObject();
+				o.put("event_id", rset.getInt("event_id"));
+				o.put("event_date",rset.getString("event_date"));
+				o.put("event_place",rset.getString("event_place"));
+				o.put("topic_name", rset.getString("topic_name"));
+				o.put("topic_category",rset.getString("topic_category"));
+				o.put("max_allowed_members", rset.getInt("max_allowed_members"));
+				arr.put(o);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} catch (JSONException e) {
+			e.printStackTrace();
+		} finally
+		{
+			try {
+				st.close();
+				conn.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		return arr.toString();
+	}
+	
+	@Path("/get/members")
+	@POST
+	@Consumes(MediaType.TEXT_PLAIN)
+	@Produces(MediaType.APPLICATION_JSON)
+	public String getMembersForEvent(String eventid)
+	{
+		JSONArray arr = new JSONArray();
+		int evid = Integer.parseInt(eventid);
+		JSONObject o;
+		Connection conn = DatabaseHandler.getConnection();
+		Statement st = null;
+		ResultSet rset;
+		
+		try {
+			st = (Statement) conn.createStatement();
+			rset = st.executeQuery("select * from users_mst " +
+					"where uname in " +
+						"(select event_members from event_attendees " +
+						"where event=" + evid + ");");
+			while(rset.next()){
+				o = new JSONObject();
+				o.put("uname", (rset.getString("uname")));
+				o.put("fname", (rset.getString("fname")));
+				o.put("lname", (rset.getString("lname")));
+				o.put("dob", (rset.getString("dob")));
+				o.put("email", (rset.getString("email")));
+				o.put("phone", (rset.getString("edu")));
+				o.put("work", (rset.getString("work")));
+				o.put("rating", (rset.getString("rating")));
+				arr.put(o);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} catch (JSONException e) {
+			e.printStackTrace();
+		} finally
+		{
+			try {
+				st.close();
+				conn.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		return arr.toString();
+	}
+	
+	@Path("/isMember")
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.TEXT_PLAIN)
+	public String isMember(String req)
+	{
+		String uname = null;
+		int eventid = 0;
+		try {
+			JSONObject o = new JSONObject(req);
+			uname = o.getString("uname");
+			eventid = o.getInt("event_id");
+		} catch (JSONException e) {
+			e.printStackTrace();
+			return Constants.RESP_MALFORMED;
+		}
+		
+		Connection conn = DatabaseHandler.getConnection();
+		Statement st = null;
+		ResultSet rset;
+		
+		try {
+			st = (Statement) conn.createStatement();
+			rset = st.executeQuery("select * from event_attendees " +
+					"where event="+ eventid + " and event_members='" + uname + "';" );
+			if(rset.next())
+				return Constants.RESP_YES;
+			else
+				return Constants.RESP_NO;
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally
+		{
+			try {
+				st.close();
+				conn.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
+		return Constants.RESP_NO;
 	}
 }
